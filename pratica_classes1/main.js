ea9d919836be24fa66a2074f9f710fc372b0ae40
pier-ex <nathanaelrog@hotmail.com>
2026-09-08 18:29:54 -0300

    study: classes, herança, polimorfismo e encapsulamento

--- pratica_classes1/main.js
+++ pratica_classes1/main.js
@@ -177,10 +177,230 @@
 
 
 
-class Calculadora {
-    static somar(a,b) {
-        return a +  b
+// class Calculadora {
+//     static somar(a,b) {
+//         return a +  b
+//     }
+// }
+
+// console.log(Calculadora.somar(10,20))
+
+
+// class Calculadora {
+//     static somar(a,b) {
+//         return a + b
+//     }
+
+//     static subtrair(a,b) {
+//         return a - b
+//     }
+
+//     multiplicar(a,b) {
+//         return a * b
+//     }
+
+//     dividir(a,b) {
+//         if (b === 0) {
+//             console.log('não é possivel dividir por 0')
+//             return
+//         } else {
+//             return a / b
+//         }
+//     }
+// }
+
+// const calculadora = new Calculadora()
+
+// console.log(Calculadora.somar(10, 5))
+// console.log(Calculadora.subtrair(10, 5))
+
+// console.log(calculadora.multiplicar(10, 5))
+// console.log(calculadora.dividir(10, 5))
+// console.log(calculadora.dividir(10, 0))
+
+
+
+// class Animal {
+//     constructor(nome) {
+//         this.nome = nome
+//     }
+
+//     emitirSom() {
+//         console.log('o animal emitiu um som')
+//     }
+// }
+
+// class Cachorro extends Animal {
+//     constructor(nome, raca) {
+//         super(nome)
+
+//         this.raca = raca
+//     }
+
+//     latir() {
+//         console.log(`${this.nome} está latindo`)
+//     }
+// }
+
+// const cachorro1 = new Cachorro('rex', 'pastor alemão')
+
+// console.log(cachorro1.nome)
+// cachorro1.emitirSom()
+
+
+// class Pessoa {
+//     constructor(nome, idade) {
+//         this.nome = nome
+//         this.idade = idade
+//     }
+
+//     apresentar() {
+//         console.log(`olá, meu nome é ${this.nome} e tenho ${this.idade} anos`)
+//     }
+// }
+
+// const pessoa1 = new Pessoa('joão', 26)
+
+// class Funcionario extends Pessoa {
+//     constructor(nome, idade, cargo, salario) {
+//         super(nome, idade)
+//         this.cargo = cargo
+//         this.salario = salario
+//     }
+
+//     mostrarCargo() {
+//         console.log(`${this.nome} trabalha como ${this.cargo}`)
+//     }
+
+//     apresentar() {
+//         super.apresentar() 
+//             console.log(`olá, meu nome é ${this.nome} e eu sou ${this.cargo}`)
+//     }
+// }
+
+// pessoa1.apresentar()
+
+// const funcionario1 = new Funcionario('joão', 26, 'Desenvolvedor', 4100)
+
+// funcionario1.apresentar()
+// funcionario1.mostrarCargo()
+
+// class Gerente extends Funcionario {
+//     constructor(nome, idade, cargo, salario, departamento) {
+//         super(nome, idade, cargo, salario) 
+//         this.departamento = departamento
+//     }
+
+//     mostrarDepartamento() {
+//         console.log(`${this.nome} é ${this.cargo} do departamento de ${this.departamento}`)
+//     }
+// }
+
+// const gerente1 = new Gerente('Carlos', 35, 'Gerente', 8000, 'TI')
+
+// gerente1.apresentar()
+// gerente1.mostrarCargo()
+// gerente1.mostrarDepartamento()
+
+// class Animal {
+//     constructor(nome) {
+//         this.nome = nome
+//     }
+
+//     emitirSom() {
+//         console.log(`${this.nome} emitiu um som`)
+//     }
+// }
+
+
+// class Cachorro extends Animal {
+//     constructor(nome) {
+//         super(nome)
+//     }
+
+
+//     emitirSom() {
+//         console.log(`${this.nome} faz: Au Au`)
+//     }
+// }
+
+// class Gato extends Animal {
+//     constructor(nome) {
+//         super(nome)
+//     }
+
+//     emitirSom() {
+//         console.log(`${this.nome} faz: Miau!`)
+//     }
+// }
+
+
+// const cachorro1 = new Cachorro('rex')
+// const gato1 = new Gato('mimi')
+
+
+// gato1.emitirSom()
+// cachorro1.emitirSom()
+
+
+// class Conta {
+//     constructor(nome, saldo) {
+//         this.nome = nome
+//         this.saldo = saldo
+//     }
+// }
+
+// const conta = new Conta('joão', 1000)
+
+
+
+// class Conta {
+//     #saldo = 0 
+
+//     depositar(valor) {
+//         if (valor > 0) {
+//             this.#saldo += valor
+//         }
+//     }
+
+//     sacar(valor) {
+//         if ( valor > 0 && valor > this.#saldo) {
+//             this.#saldo -= valor
+//         }
+//     }
+
+//     mostrarSaldo() {
+//         console.log(`saldo : R$ ${this.#saldo}`)
+//     }
+// }
+
+
+
+class ContaBancaria {
+    #saldo = 0
+
+    depositar(valor) {
+        if (valor > 0) {
+            this.#saldo += valor
+        }
+    } 
+
+    get saldo() {
+        return this.#saldo
+    }
+
+    sacar(valor) {
+        if (valor > 0 && this.#saldo >= valor) {
+            this.#saldo -= valor
+        }
     }
 }
 
-console.log(Calculadora.somar(10,20))
+
+const conta1 = new ContaBancaria() 
+
+    conta1.depositar(500)
+    conta1.sacar(300)
+
+console.log(conta1.saldo)
+console.log(conta1.#saldo)
